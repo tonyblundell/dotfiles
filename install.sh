ddd59423706e11b801bdf4afca937b2dedd495bf
Tony Blundell <tonyblundell@gmail.com>
2015-12-15 14:34:07 +0000

    Backup to single directory

--- install.sh
+++ install.sh
@@ -1,32 +1,18 @@
-now=`date +"%s"`
-pwd=`echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"`
+# Make backup dir
+backupdir="backup/$(date +%s)/"
+mkdir -p $backupdir
 
-echo 'Installing bash config'
-mv ~/.bashrc ~/.bashrc.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/bashrc ~/.bashrc
+# Backup then create a symlink for each dotfile
+for dotfile in dotfiles/*
+do
+    basename="$(basename $dotfile)"
+    mv "$HOME/.$basename" "$backupdir/$basename"
+    ln -s $(readlink -f $dotfile) "$HOME/.$basename"
+done
 
-mv ~/.inputrc ~/.inputrc.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/inputrc ~/.inputrc
-
-mv ~/.git-prompt.sh ~/.git-prompt.sh.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/git-prompt.sh ~/.git-prompt.sh
-
-echo 'Installing git config'
-mv ~/.gitconfig ~/.gitconfig.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/gitconfig ~/.gitconfig
-
-echo 'Installing tmux config'
-mv ~/.tmux.conf ~/.tmux.conf.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/tmux.conf ~/.tmux.conf
-
-echo 'Installing vim config'
-mv ~/.vim/vimrc ~/.vim/vimrc.old.$now 2>/dev/null
-mv ~/.vimrc ~/.vimrc.old.$now 2>/dev/null
-ln -s "$pwd"dotfiles/vimrc ~/.vimrc
-
-echo 'Installing vim bundles'
-mv ~/.vim/bundle ~/.vim/bundle.old.$now 2>/dev/null
-mkdir -p ~/.vim/bundle 2>/dev/null
-for repo in `cat vimbundles.txt`; do
+# Install Vim bundles
+mkdir -p ~/.vim/bundle
+for repo in $(cat vimbundles.txt)
+do
     cd ~/.vim/bundle && git clone $repo
 done

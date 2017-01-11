380381dafc32c89b84da3cc989abc6669bcac85e
Tony Blundell <tonyblundell@gmail.com>
2017-01-11 11:59:37 +0000

    Clean up installer

--- install.sh
+++ install.sh
@@ -7,14 +7,18 @@ mkdir -p $backupdir
 # Backup then create a symlink for each dotfile
 for dotfile in dotfiles/*
 do
+    echo "DOTFILES - Installing $dotfile"
     basename="$(basename $dotfile)"
     mv "$HOME/.$basename" "$backupdir/$basename"
     ln -s "$repodir/$dotfile" "$HOME/.$basename"
 done
 
 # Install Vim bundles
+echo "DOTFILES - Installing vim bundles"
 mkdir -p ~/.vim/bundle
+rm -rf ~/.vim/bundle/*
 for repo in $(cat vimbundles.txt)
 do
     cd ~/.vim/bundle && git clone $repo
 done
+echo "DOTFILES - Done"
